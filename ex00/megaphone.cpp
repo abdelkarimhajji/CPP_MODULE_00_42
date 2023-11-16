f8f5a1a974b719da9b56f4c4525761464e34610a
abdelkarim hajji <abdelkarim.hajji.2001@gmail.com>
2023-11-16 11:09:38 +0100

    i create first exercice in cpp00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int ac, char **av) {
+    int i = 0;
+    int j = 0;
+
+    while (i < ac)
+    {
+        j = 0;
+        while (av[i][j])
+        {
+            if(av[i][j] >='a' && av[i][j] <= 'z')
+                av[i][j] -= 32;
+            std::cout << av[i][j];
+            j++;
+        }
+         std::cout << std::endl;
+        i++;
+    }
+    return 0;
+}
